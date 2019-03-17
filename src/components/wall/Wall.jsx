698eae3121948761c58b02bf0a25b1cca7b4c160
Vitalii <vitalii.potapov92@gmail.com>
2019-03-17 16:55:52 +0600

    Урок 15, проработка компонента стены и постов

--- src/components/wall/Wall.jsx
+++ src/components/wall/Wall.jsx
@@ -5,6 +5,9 @@ import './Wall.css';
 function Wall() {
   return (
     <div className="wall">
+      <textarea className="wall__input" name="posts" id="" cols="30" rows="5" />
+      <button className="wall__button" type="button">Add post</button>
+      <hr />
       <Posts />
     </div>
   );
--- src/components/wall/posts/Posts.jsx
+++ src/components/wall/posts/Posts.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Post from './post/Post';
 import './Posts.css';
 
 function Posts() {
   return (
     <div className="posts">
-      <div className="posts__item">Post #1</div>
-      <div className="posts__item">Post #2</div>
+      <Post />
     </div>
   );
 }
